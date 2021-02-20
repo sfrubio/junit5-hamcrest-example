8ca618375c3a759e37fe7b09b0e041430714332c
SERGIO FRANCISCO RUBIO <sergio.rubio@viavarejo.com.br>
2021-02-20 15:40:23 -0300

    add more descriptive messages on Application class

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,9 +1,9 @@
 public class Application {
     public static void main(String[] args) {
         Refrigerator refrigerator = new Refrigerator();
-        System.out.println(refrigerator.whatDoIHaveOnFirstRow());
-        System.out.println(refrigerator.whatDoIHaveOnRow(2));
-        System.out.println(refrigerator.doIHaveAn("cheese"));
-        System.out.println(refrigerator.howManyRowsDoItHas());
+        System.out.println("What do I have on first row?: " + refrigerator.whatDoIHaveOnFirstRow());
+        System.out.println("What do I have on 2nd row? " + refrigerator.whatDoIHaveOnRow(2));
+        System.out.println("Do I have a cheese? " + (refrigerator.doIHaveAn("cheese") ? "yes" : "no"));
+        System.out.println("How many rows do it has?: " + refrigerator.howManyRowsDoItHas());
     }
 }
